448ef6a95ce50f59e8d21626003126d9535cdabc
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-12 21:12:24 +0100

    Php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <title>Partie 2 : Exercice 8</title>
+</head>
+<body>
+  <?php
+  $variable = true;
+
+  if ($variable) {
+    echo 'C\'est ok !!';
+  }
+  else {
+    'C\'est pas bon !!!';
+  }
+  ?>
+</body>
+</html>
